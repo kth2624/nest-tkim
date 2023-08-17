af4e4b493580d3c6d6c611900db674c12c975b9b
Intra id : tkim <39645477+kth2624@users.noreply.github.com>
2023-08-18 01:01:22 +0900

    practice nestjs CRUD

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { BoardService } from './board.service';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
+@Controller('boards')
+export class BoardController {
+  constructor(private boardService: BoardService) {}
 
-@Controller('board')
-export class BoardController {}
+  @Get('/')
+  getBoard(): Board[] {
+    return this.boardService.getBoard();
+  }
+
+  @Post('/')
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string) {
+    return this.boardService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardService.deleteBoard(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ) {
+    return this.boardService.updateBoardStatus(id, status);
+  }
+}
