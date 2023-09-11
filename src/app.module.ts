41dff335d2e0ee3d3725cf9ca9a89313fdcc3094
Intra id : tkim <39645477+kth2624@users.noreply.github.com>
2023-09-11 22:30:24 +0900

    feat: board test 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as path from 'path';
 
+console.log(__dirname);
 @Module({
-  imports: [BoardModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: +process.env.MYSQL_PORT,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [path.join(__dirname, '/entities/**.entity{.ts,.js}')],
+      synchronize: false,
+    }),
+    BoardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/board/board.controller.spec.ts
+++ src/board/board.controller.spec.ts
@@ -1,18 +1,73 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BoardController } from './board.controller';
+import { BoardService } from './board.service';
+import { BoardsEntity } from '../entities/board.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 describe('BoardController', () => {
   let controller: BoardController;
-
+  let service: BoardService;
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const module = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forFeature([BoardsEntity]),
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: process.env.MYSQL_HOST,
+          port: +process.env.MYSQL_PORT,
+          username: process.env.MYSQL_USERNAME,
+          password: process.env.MYSQL_PASSWORD,
+          database: process.env.MYSQL_DATABASE,
+          entities: [BoardsEntity],
+          synchronize: false,
+        }),
+      ],
       controllers: [BoardController],
+      providers: [BoardService],
     }).compile();
 
     controller = module.get<BoardController>(BoardController);
+    service = module.get<BoardService>(BoardService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getBoardById', () => {
+    it('should call the service', async () => {
+      const result = {
+        id: 2,
+        title: 'board2',
+        description: 'hello',
+        createdAt: new Date('2023-09-08T23:18:53.000Z'),
+        updatedAt: new Date('2023-09-08T23:18:53.000Z'),
+      };
+      const spy = jest.spyOn(service, 'getBoardById').mockResolvedValue(result);
+      expect(await controller.getBoardById('2')).toStrictEqual(result); //실제 디비에 있는 값과 비교
+      expect(spy).toBeCalledWith(2);
+    });
+  });
+
+  describe('createBoard', () => {
+    it('should call the service', async () => {
+      const result = {
+        id: 1,
+        title: 'test title',
+        description: 'hello world',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      const spy = jest.spyOn(service, 'createBoard').mockResolvedValue(result);
+      expect(
+        await controller.createBoard({
+          title: result.title,
+          description: result.description,
+        }),
+      ).toStrictEqual(result);
+      //expect(spy).toBeCalledWith(result);
+    });
   });
 });
